79d2a3f89f93ccfba428daaf968212d7ba110198
manchandajayant <maanjayant@gmail.com>
2020-05-20 10:18:03 +0200

    user funnctions and class defined

--- src/pofiles_project/profiles_api/models.py
+++ src/pofiles_project/profiles_api/models.py
@@ -1,10 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
-
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 
+class UserProfileManager(BaseUserManager):
+    """helps django work with  out custom user models"""
+
+    def create_user(self,email,name,password=None):
+        """creates a new user profile object"""
+
+        if not email:
+            raise ValueError('users must have an email address.')
+
+        email = self.normalize_email(email)
+        user =  self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuserr(self,email,name,password):
+        """creates and saves a new superuser with given details"""
+
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self_.db)
+
+        return user 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Represent a user profile"""
     email = models.EmailField(max_length=255,unique=True)
